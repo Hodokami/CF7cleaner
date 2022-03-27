767f965ddaa64231f91857ea8bf7e86fdc4ed84e
Hiroki Nakano <h.n.keigyaku.mac@gmail.com>
2022-03-28 05:17:29 +0900

    Update cf7cleaner.php
    Add slag option setting function.

--- cf7cleaner.php
+++ cf7cleaner.php
@@ -2,12 +2,28 @@
 /*
 	Plugin Name: CF7 JS&CSS Cleaner
 	Description: Clean JS&CSS of "Contact Form 7" on Pages other than Form page. THIS PLUGIN CANNOT ACTIVATE WHEN "Contact Form 7" IS DISABLED!
-	Version: 0.1
+	Version: 0.2
 */
 if ( ! defined( 'ABSPATH' ) ) exit; // This php needs load wp-config.php
 
+$cf7cslag = cf7c_get();
+
+if(false === $cf7cslag)//check option existence
+{
+	$init_cf7c = array();
+	cf7c_set($init_cf7c);
+}
+function cf7c_get()
+{
+	return get_option('tellaresdo_cf7c_slag');
+}
+function cf7c_set($cf7cslag)
+{
+	update_option ('tellaresdo_cf7c_slag', $cf7cslag, 'no');
+}
+
 add_filter('pre_update_option_active_plugins', 'cleaner_load_before_cf7', 10, 2); // plugin enabled/disabled hook for 'cleaner_load_before_cf7'
-function cleaner_load_before_cf7( $active_plugins, $old_value )
+function cleaner_load_before_cf7($active_plugins, $old_value)
 /*
 	Set cf7cleaner load before CF7.
 	<!> WHEN CF7 IS DISABLED, 'cleaner_load_before_cf7' REMOVE "cf7cleaner" FROM "active_plugins"!
@@ -45,4 +61,32 @@ function cleaner_load_before_cf7( $active_plugins, $old_value )
 	}
 	$active_plugins = array_values($active_plugins); // reset array key
 	return $active_plugins;
+}
+
+add_filter('wp_insert_post_data', 'cf7cleaner_checker', 10, 1);
+function cf7cleaner_checker($data) // set Option value
+{
+	$cf7cslag = cf7c_get();
+	$post_slag = $data['post_name'];
+	$cf7c_optscan = in_array($post_slag, $cf7cslag);
+	$cf7_match = preg_match('/\[contact-form-7 id=(.+?) title=(.+?)\]/', $data['post_content']);
+	if (1 === $cf7_match && false === $cf7c_optscan)
+	{
+		$cf7cslag[] = $post_slag;
+		cf7c_set($cf7cslag);
+	}
+	if (0 === $cf7_match && false !== $cf7c_optscan)
+	{
+		foreach ($cf7cslag as $no=>$slag)
+		{
+			if ($slag == $post_slag) // REMOVER
+				{
+					unset($cf7cslag[$no]);
+					$cf7cslag = array_values($cf7cslag);
+					cf7c_set($cf7cslag);
+				}
+		
+		}
+	}
+	return $data;
 }
